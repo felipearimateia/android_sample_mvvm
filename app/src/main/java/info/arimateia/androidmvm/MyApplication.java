b84b3259a511b4cb947d083cc072283ece5a3ac6
Felipe Arimateia <felipets@ciandt.com>
2016-08-23 19:05:27 -0300

    Update Dagger

--- app/src/main/java/info/arimateia/androidmvm/MyApplication.java
+++ app/src/main/java/info/arimateia/androidmvm/MyApplication.java
@@ -6,6 +6,7 @@ import info.arimateia.androidmvm.injection.AppComponent;
 import info.arimateia.androidmvm.injection.BusinessModule;
 import info.arimateia.androidmvm.injection.DaggerAppComponent;
 import info.arimateia.androidmvm.injection.RemoteModule;
+import info.arimateia.androidmvm.injection.RepositoryModule;
 
 /**
  * Created by felipets on 8/23/16.
@@ -22,6 +23,7 @@ public class MyApplication extends Application {
         appComponent = DaggerAppComponent.builder()
                 .businessModule(new BusinessModule())
                 .remoteModule(new RemoteModule())
+                .repositoryModule(new RepositoryModule())
                 .build();
 
         AppContext.setComponent(appComponent);
